cdc6ce6391ce0d3275ccb3e7da773f5e4a57ec72
Jaspreet Singh <JaspreetRFSingh@users.noreply.github.com>
2019-07-23 02:24:44 +0530

    Stochastic gradient

--- maths_concepts/gradient_descent.py
+++ maths_concepts/gradient_descent.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from collections import Counter
-from linear_algebra import distance, vector_subtract, scalar_multiply
+from vector_operations import distance, vector_subtract, scalar_multiply
 from functools import reduce
 import math, random
 
@@ -41,3 +41,101 @@ def safe(f):
         except:
             return float('inf')
     return safe_f
+
+
+
+
+#
+#
+# minimize / maximize batch
+#
+#
+
+def minimize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
+    """use gradient descent to find theta that minimizes target function"""
+
+    step_sizes = [100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+
+    theta = theta_0                           # set theta to initial value
+    target_fn = safe(target_fn)               # safe version of target_fn
+    value = target_fn(theta)                  # value we're minimizing
+
+    while True:
+        gradient = gradient_fn(theta)
+        next_thetas = [step(theta, gradient, -step_size)
+                       for step_size in step_sizes]
+
+        # choose the one that minimizes the error function
+        next_theta = min(next_thetas, key=target_fn)
+        next_value = target_fn(next_theta)
+
+        # stop if we're "converging"
+        if abs(value - next_value) < tolerance:
+            return theta
+        else:
+            theta, value = next_theta, next_value
+
+def negate(f):
+    """return a function that for any input x returns -f(x)"""
+    return lambda *args, **kwargs: -f(*args, **kwargs)
+
+def negate_all(f):
+    """the same when f returns a list of numbers"""
+    return lambda *args, **kwargs: [-y for y in f(*args, **kwargs)]
+
+def maximize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
+    return minimize_batch(negate(target_fn),
+                          negate_all(gradient_fn),
+                          theta_0,
+                          tolerance)
+
+#
+# minimize / maximize stochastic
+#
+
+def in_random_order(data):
+    """generator that returns the elements of data in random order"""
+    indexes = [i for i, _ in enumerate(data)]  # create a list of indexes
+    random.shuffle(indexes)                    # shuffle them
+    for i in indexes:                          # return the data in that order
+        yield data[i]
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+
+    data = list(zip(x, y))
+    theta = theta_0                             # initial guess
+    alpha = alpha_0                             # initial step size
+    min_theta, min_value = None, float("inf")   # the minimum so far
+    iterations_with_no_improvement = 0
+
+    # if we ever go 100 iterations with no improvement, stop
+    while iterations_with_no_improvement < 100:
+        value = sum( target_fn(x_i, y_i, theta) for x_i, y_i in data )
+
+        if value < min_value:
+            # if we've found a new minimum, remember it
+            # and go back to the original step size
+            min_theta, min_value = theta, value
+            iterations_with_no_improvement = 0
+            alpha = alpha_0
+        else:
+            # otherwise we're not improving, so try shrinking the step size
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+
+        # and take a gradient step for each of the data points
+        for x_i, y_i in in_random_order(data):
+            gradient_i = gradient_fn(x_i, y_i, theta)
+            theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
+
+    return min_theta
+
+def maximize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+    return minimize_stochastic(negate(target_fn),
+                               negate_all(gradient_fn),
+                               x, y, theta_0, alpha_0)
+    
+v = [random.randint(-10,10) for i in range(3)]
+v = minimize_batch(sum_of_squares, sum_of_squares_gradient, v)
+print("minimum v", v)
+print("minimum value", sum_of_squares(v))
