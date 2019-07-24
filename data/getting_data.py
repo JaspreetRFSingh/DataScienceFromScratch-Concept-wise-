2822edff8d5a7c969db27e524ea0206a0e0257f3
Jaspreet Singh <JaspreetRFSingh@users.noreply.github.com>
2019-07-25 02:25:03 +0530

    Fooling around with my parsed github repos

--- data/getting_data.py
+++ data/getting_data.py
@@ -3,6 +3,7 @@ from collections import Counter
 import math, random, csv, json, re
 from bs4 import BeautifulSoup
 import requests
+from matplotlib import pyplot as plt
 
 def process(date, symbol, price):
         print(date, symbol, price)
@@ -56,21 +57,11 @@ print(first_paragraph)
 first_paragraph_text = soup.p.text
 first_paragraph_words = soup.p.text.split()
 
-
-assert first_paragraph_words == ['This', 'is', 'the', 'first', 'paragraph.']
-
 first_paragraph_id = soup.p['id']       # raises KeyError if no 'id'
 first_paragraph_id2 = soup.p.get('id')  # returns None if no 'id'
 
-
-assert first_paragraph_id == first_paragraph_id2 == 'p1'
-
 all_paragraphs = soup.find_all('p')  # or just soup('p')
 paragraphs_with_ids = [p for p in soup('p') if p.get('id')]
-
-
-assert len(all_paragraphs) == 2
-assert len(paragraphs_with_ids) == 1
 
 important_paragraphs = soup('p', {'class' : 'important'})
 important_paragraphs2 = soup('p', 'important')
@@ -78,7 +69,139 @@ important_paragraphs3 = [p for p in soup('p')
                          if 'important' in p.get('class', [])]
 
 
-assert important_paragraphs == important_paragraphs2 == important_paragraphs3
-assert len(important_paragraphs) == 1
+
+######
+#
+# BOOKS ABOUT DATA
+#
+######
+
+#def is_video(td):
+#    """it's a video if it has exactly one pricelabel, and if
+#    the stripped text inside that pricelabel starts with 'Video'"""
+#    pricelabels = td('span', 'pricelabel')
+#    return (len(pricelabels) == 1 and
+#            pricelabels[0].text.strip().startswith("Video"))
+#
+#def book_info(td):
+#    """given a BeautifulSoup <td> Tag representing a book,
+#    extract the book's details and return a dict"""
+#    
+#    title = td.find("div", "thumbheader").a.text
+#    by_author = td.find('div', 'AuthorName').text
+#    authors = [x.strip() for x in re.sub("^By ", "", by_author).split(",")]
+#    isbn_link = td.find("div", "thumbheader").a.get("href")
+#    isbn = re.match("/product/(.*)\.do", isbn_link).groups()[0]
+#    date = td.find("span", "directorydate").text.strip()
+#    
+#    return {
+#        "title" : title,
+#        "authors" : authors,
+#        "isbn" : isbn,
+#        "date" : date
+#    }
+
+#from time import sleep
+#
+#base_url = "http://shop.oreilly.com/category/browse-subjects/" + \
+#       "data.do?sortby=publicationDate&page="
+
+books = []
+
+books.append({"title":"Data Science from scratch", "year":2014})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2017})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2016})
+books.append({"title":"Data Science from scratch", "year":2014})
+books.append({"title":"Data Science from scratch", "year":2014})
+books.append({"title":"Data Science from scratch", "year":2014})
+books.append({"title":"Data Science from scratch", "year":2014})
+books.append({"title":"Data Science from scratch", "year":2013})
+books.append({"title":"Data Science from scratch", "year":2013})
+books.append({"title":"Data Science from scratch", "year":2013})
+books.append({"title":"Data Science from scratch", "year":2013})
+books.append({"title":"Data Science from scratch", "year":2012})
+books.append({"title":"Data Science from scratch", "year":2012})
+books.append({"title":"Data Science from scratch", "year":2012})
+books.append({"title":"Data Science from scratch", "year":2012})
+books.append({"title":"Data Science from scratch", "year":2011})
+books.append({"title":"Data Science from scratch", "year":2011})
+books.append({"title":"Data Science from scratch", "year":2011})
+books.append({"title":"Data Science from scratch", "year":2011})
+books.append({"title":"Data Science from scratch", "year":2010})
+books.append({"title":"Data Science from scratch", "year":2010})
+books.append({"title":"Data Science from scratch", "year":2010})
+books.append({"title":"Data Science from scratch", "year":2009})
+
+#for page_num in range(1, 31 + 1):
+#    print ("souping page", page_num)
+#    url = base_url + str(page_num)
+#    soup = BeautifulSoup(requests.get(url).text, 'html5lib')
+#        
+#    for td in soup('td', 'thumbtext'):
+#        if not is_video(td):
+#            books.append(book_info(td))
+#            
+#    print(books)
+#    # now be a good citizen and respect the robots.txt!
+#    sleep(0.5)
+
+
+def get_year(book):
+    """book["date"] looks like 'November 2014' so we need to 
+    split on the space and then take the second piece"""
+    return int(book["year"])
+
+
+    # 2014 is the last complete year of data (when I ran this)
+year_counts = Counter(get_year(book) for book in books
+                      if get_year(book) <= 2017)
+
+years = sorted(year_counts)
+print('years:', years)
+book_counts = [year_counts[year] for year in years]
+plt.bar([x - 0.5 for x in years], book_counts)
+plt.xlabel("year")
+plt.ylabel("# of data books")
+plt.title("Data is Big!")
+plt.show()
+
+
+#Using APIs
+
+#parsing my github repo
+
+endpoint = "https://api.github.com/users/jaspreetRFSingh/repos"
+
+repos = json.loads(requests.get(endpoint).text)
+num_of_repos = len(repos)
+for i in range(num_of_repos):    
+    print(repos[i]["name"])
+
+from dateutil.parser import parse
+
+
+dates = [parse(repo["created_at"]) for repo in repos]
+month_counts = Counter(date.month for date in dates)
+weekday_counts = Counter(date.weekday() for date in dates)
+
+print(month_counts, weekday_counts)
 
 
