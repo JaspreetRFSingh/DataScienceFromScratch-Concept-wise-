10b91be46da0e824c8fe4cf00cafc45a311af040
Jaspreet Singh <JaspreetRFSingh@users.noreply.github.com>
2019-07-27 01:29:43 +0530

    Added matrix_util.py

---
+++ maths_concepts/matrix_util.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+def shape(A):
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0
+    return num_rows, num_cols
+
+def get_row(A, i):
+    return A[i]
+
+def get_column(A, j):
+    return [A_i[j] for A_i in A]
+
+def make_matrix(num_rows, num_cols, entry_fn):
+    """returns a num_rows x num_cols matrix
+    whose (i,j)-th entry is entry_fn(i, j)"""
+    return [[entry_fn(i, j) for j in range(num_cols)]
+            for i in range(num_rows)]
+
+def is_diagonal(i, j):
+    """1's on the 'diagonal', 0's everywhere else"""
+    return 1 if i == j else 0
+
+identity_matrix = make_matrix(5, 5, is_diagonal)
+
+#          user 0  1  2  3  4  5  6  7  8  9
+#
+friendships = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0], # user 0
+               [1, 0, 1, 1, 0, 0, 0, 0, 0, 0], # user 1
+               [1, 1, 0, 1, 0, 0, 0, 0, 0, 0], # user 2
+               [0, 1, 1, 0, 1, 0, 0, 0, 0, 0], # user 3
+               [0, 0, 0, 1, 0, 1, 0, 0, 0, 0], # user 4
+               [0, 0, 0, 0, 1, 0, 1, 1, 0, 0], # user 5
+               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0], # user 6
+               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0], # user 7
+               [0, 0, 0, 0, 0, 0, 1, 1, 0, 1], # user 8
+               [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]] # user 9
+
+#####
+# DELETE DOWN
+#
+
+
+def matrix_add(A, B):
+    if shape(A) != shape(B):
+        raise ArithmeticError("cannot add matrices with different shapes")
+
+    num_rows, num_cols = shape(A)
+    def entry_fn(i, j): return A[i][j] + B[i][j]
+
+    return make_matrix(num_rows, num_cols, entry_fn)
+
