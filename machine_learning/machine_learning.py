a24bc1219b3b51e545b8faa3724764d3814ffdd3
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2019-08-26 14:21:51 +0530

    Correctness of a model

--- machine_learning/machine_learning.py
+++ machine_learning/machine_learning.py
@@ -22,3 +22,29 @@ def train_test_split(x, y, test_pct):
     x_train, y_train = list(zip(*train))
     x_test, y_test = list(zip(*test))
     return x_train, x_test, y_train, y_test
+
+#Correctness
+
+def accuracy(true_positive, false_positive, false_negative, true_negative):
+    return (true_positive + true_negative)/(true_positive + false_positive + false_negative +true_negative)
+
+#precision measures the accuracy of positive predictions
+def precision(tp,fp,fn,tn):
+    return tp/(tp+fp)
+#recall measures fractions of postives
+def recall(tp,fp,fn, tn):
+    return tp / (tp+fn)
+
+#f1 score is the harmonic mean of recall and precision
+def f1_score(tp, fp, fn, tn):
+    p = precision(tp, fp, fn, tn)
+    r = recall(tp, fp, fn, tn)
+    return 2 * p * r / (p + r)
+
+
+print("accuracy(70, 4930, 13930, 981070)", accuracy(70, 4930, 13930, 981070))
+print("precision(70, 4930, 13930, 981070)", precision(70, 4930, 13930, 981070))
+print("recall(70, 4930, 13930, 981070)", recall(70, 4930, 13930, 981070))
+print("f1_score(70, 4930, 13930, 981070)", f1_score(70, 4930, 13930, 981070))
+
+
